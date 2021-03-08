57262cbe9820e79d61bffed6c97f5d21de6ec566
kaquino1 <59567712+kaquino1@users.noreply.github.com>
2021-03-08 14:10:56 -0500

    Update database.js

--- database.js
+++ database.js
@@ -11,7 +11,7 @@ app.use(CORS());
 
 app.get("/", (req, res, next) => {
     res.setHeader('Content-type','text/html');
-    res.send("<p>Server for <a hef='https://kaquino1.github.io/clinic-website'>https://kaquino1.github.io/clinic-website</a></p>");
+    res.send("<a hef='https://kaquino1.github.io/clinic-website'>Server for https://kaquino1.github.io/clinic-website</a>");
 
 });
 
